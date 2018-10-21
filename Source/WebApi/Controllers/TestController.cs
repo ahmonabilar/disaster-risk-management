37908cd91fee241867928c282893b2c1fa42c558
Andamon Abilar <ahmon_abilar@yahoo.com>
2018-10-21 08:17:33 +0800

    Add WebApi project

---
+++ Source/WebApi/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+  [Route("api/[Controller]")]
+  public class TestController : Controller
+  {
+    [HttpGet]
+    public IEnumerable<User> Get()
+    {
+      var users = new List<User>
+      {
+        new User { Id = 1, Username = "User1", Password = "Pass1" },
+        new User { Id = 2, Username = "User2", Password = "Pass2" },
+        new User { Id = 3, Username = "User3", Password = "Pass3" },
+        new User { Id = 4, Username = "User4", Password = "Pass4" },
+      };
+
+      return users;
+    }
+  }
+
+  public class User
+  {
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+  }
+}
