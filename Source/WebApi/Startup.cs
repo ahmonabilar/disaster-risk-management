4b49ac1a08692cc70158f22e5ae7c39d608c32be
ahmonabilar <ahmon_abilar@yahoo.com>
2018-10-23 15:54:49 +0800

    Dynamic Assembly name

--- Source/WebApi/Startup.cs
+++ Source/WebApi/Startup.cs
@@ -8,46 +8,46 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Drm.WebApi
 {
     public class Startup
-  {
-    private readonly IConfiguration _config;
-
-    public Startup(IConfiguration config)
     {
-      _config = config;
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DRMContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DRMConnectionString"), opt => opt.MigrationsAssembly(typeof(Startup).Namespace));
+            });
+
+            services.AddTransient<DRMSeeder>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+            "/{controller}/{action}/{id?}",
+            new { Controller = "App", Action = "Index" });
+            });
+
+
+        }
     }
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DRMContext>(cfg =>
-      {
-        cfg.UseSqlServer(_config.GetConnectionString("DRMConnectionString"), opt => opt.MigrationsAssembly("Drm.WebApi"));
-      });
-
-      services.AddTransient<DRMSeeder>();
-
-      services.AddMvc();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      //if (env.IsDevelopment())
-      //{
-      //    app.UseDeveloperExceptionPage();
-      //}
-      app.UseDeveloperExceptionPage();
-
-      app.UseStaticFiles();
-
-      app.UseMvc(cfg => 
-      {
-        cfg.MapRoute("Default",
-          "/{controller}/{action}/{id?}",
-          new { Controller = "App", Action = "Index" });
-      });
-
-
-    }
-  }
 }
